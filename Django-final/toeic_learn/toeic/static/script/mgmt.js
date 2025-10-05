64a1c8f2d9708c7f0616bf5e734c9351a4dc5072
Minglung.Li <11046090@ntub.edu.tw>
2025-10-05 17:47:36 +0800

    Update mgmt page

--- Django-final/toeic_learn/toeic/static/script/mgmt.js
+++ Django-final/toeic_learn/toeic/static/script/mgmt.js
@@ -4,3 +4,10 @@ const sidebar = document.getElementById("sidebar");
 menuToggle.addEventListener("click", () => {
   sidebar.classList.toggle("active");
 });
+
+document.querySelectorAll('.sidebar-toggle').forEach(toggle => {
+    toggle.addEventListener('click', function() {
+        const parent = this.parentElement;
+        parent.classList.toggle('active');
+    });
+});
